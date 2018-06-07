dcf1729c362a118f254f5db4ffff95823fbd6990
Sami Fournier <sami.fournier.sf@gmail.com>
2018-06-07 07:49:38 +0200

    0,5 sec sur shake

--- js/main.js
+++ js/main.js
@@ -87,7 +87,7 @@
 
       $(this).addClass("kMoove");
       if (document.querySelectorAll(".kMoove").length == document.querySelectorAll(".coverK").length && !playOneTime) {
-          $('body').addClass("shake-hard").delay(700).queue(function(next){
+          $('body').addClass("shake-hard").delay(500).queue(function(next){
                $(this).removeClass("shake-hard");
                next();
           });
